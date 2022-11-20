aff7d4a0ae0899395a5627ce63c0a5edec66b520
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-19 22:44:09 -0300

    Sistema de ciclos e contador iniciado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ import {
 } from "./styles";
 
 const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, "vai se foder"),
+  task: zod.string().min(1, "Tente mais tarde"),
   minutesAmount: zod
     .number()
     .min(5, "o ciclo precisa ser até no minimo ate 5 minutos")
@@ -23,8 +23,18 @@ const newCycleFormValidationSchema = zod.object({
 
 type INewCicleFormatData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface ICycle {
+  id: string;
+  task:string;
+  minutesAmount:number;
+}
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<INewCicleFormatData>({
+const [cycles, setCycles] = useState<ICycle[]>([]) //estou dizendo que o estado vai armazenar uma lista de ciclos usando o termo <ICycle[]>
+const [activeCycleId, setActiveCycleId] = useState<string | null>(null)  
+const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+//amountSecondsPassed = segundos que ja se passaram desde que o activeCycles foi ativado
+const { register, handleSubmit, watch, reset } = useForm<INewCicleFormatData>({
     resolver: zodResolver(newCycleFormValidationSchema),
 
     defaultValues: {
@@ -34,9 +44,29 @@ export function Home() {
   });
 
   function handleCreateNewCycle(data: INewCicleFormatData) {
-    console.log(data);
-  }
+const id = String(new Date().getTime())
 
+    const newCycle:ICycle = {
+      id,
+      task:data.task,
+      minutesAmount:data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])//coloquei colchetes pq é um array de ciclos, e para adicionar um novo ciclo é necessario os tres pontos para que usem os possiveis ciclos anteriores e depois crie um novo com o newCycle
+    //toda vez que eu estou alterando um estado, e esse estado depende da sua informação anterior, é bom o estado ser setado em formato de função, assim como esta a arrow function acima
+   //state = Estado atual ( poderia ser qualquer nome)
+   setActiveCycleId(id)
+    reset()
+  }
+const activeCycles = cycles.find((cycle) => cycle.id===activeCycleId)
+console.log(activeCycles)
+
+const TotalSeconds = activeCycles? activeCycles.minutesAmount * 60: 0;
+const currentSeconds = activeCycles?TotalSeconds-amountSecondsPassed:0;
+const minutesAmount = Math.floor(currentSeconds /60);
+const secondsAmount = currentSeconds%60;
+const minutes = String(minutesAmount).padStart(2,'0');
+const seconds = String(secondsAmount).padStart(2,'0');
   const task = watch("task");
 
   const isSubmitDisable = !task;
@@ -75,11 +105,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisable} type="submit">
