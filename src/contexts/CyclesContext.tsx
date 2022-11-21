9943ca30a94ce389ded3eb9b5b6ab0ecc1c2482e
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-21 17:56:36 -0300

    Criuado pasta contexto e passado nas rotas

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,103 @@
+import { createContext, ReactNode, useState } from "react";
+interface ICreateCycleData {
+  task: string;
+  minutesAmount: number;
+}
+
+interface ICycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptDate?: Date;
+  finishedDate?: Date;
+}
+interface ICyclesContextData {
+  cycles: ICycle[];
+  activeCycles: ICycle | undefined;
+  activeCycleId: string | null;
+  markCurrentCycleAsFinished: () => void;
+  amountSecondsPassed: number;
+  setSecondsPassed: (seconds: number) => void;
+  createNewCycle: (data: ICreateCycleData) => void;
+  interruptCurrentCycle: () => void;
+}
+interface ICyclesContextProviderProps {
+  children: ReactNode;
+}
+
+export const CyclesContext = createContext({} as ICyclesContextData);
+
+export function CyclesContextProvider({
+  children,
+}: ICyclesContextProviderProps) {
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  //amountSecondsPassed = segundos que ja se passaram desde que o activeCycles foi ativado
+  const [cycles, setCycles] = useState<ICycle[]>([]); //estou dizendo que o estado vai armazenar uma lista de ciclos usando o termo <ICycle[]>
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
+  const activeCycles = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+  }
+
+  function createNewCycle(data: ICreateCycleData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: ICycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    setCycles((state) => [...state, newCycle]); //coloquei colchetes pq é um array de ciclos, e para adicionar um novo ciclo é necessario os tres pontos para que usem os possiveis ciclos anteriores e depois crie um novo com o newCycle
+    //toda vez que eu estou alterando um estado, e esse estado depende da sua informação anterior, é bom o estado ser setado em formato de função, assim como esta a arrow function acima
+    //state = Estado atual ( poderia ser qualquer nome)
+    setActiveCycleId(id);
+    setAmountSecondsPassed(0); // quando eu crio um novo ciclo, agora eu sempre volto a os segundos que se passaram para zero, para que o ciclo não comece de uma conta errada, do ciclo anterior
+    // reset();
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+    setActiveCycleId(null);
+  }
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycles,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { HandPalm, Play } from "phosphor-react";
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
@@ -11,24 +11,8 @@ import {
 } from "./styles";
 import { NewCycleForm } from "./components/NewCycleForm";
 import { Countdown } from "./components/Countdown";
+import { CyclesContext } from "../../contexts/CyclesContext";
 
-interface ICycle {
-  id: string;
-  task: string;
-  minutesAmount: number;
-  startDate: Date;
-  interruptDate?: Date;
-  finishedDate?: Date;
-}
-interface ICyclesContextData {
-  activeCycles: ICycle | undefined;
-  activeCycleId: string | null;
-  markCurrentCycleAsFinished: () => void;
-  amountSecondsPassed: number;
-  setSecondsPassed: (seconds: number) => void;
-}
-
-export const CyclesContext = createContext({} as ICyclesContextData);
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Tente mais tarde"),
   minutesAmount: zod
@@ -40,11 +24,8 @@ const newCycleFormValidationSchema = zod.object({
 type INewCicleFormatData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-  //amountSecondsPassed = segundos que ja se passaram desde que o activeCycles foi ativado
-  const [cycles, setCycles] = useState<ICycle[]>([]); //estou dizendo que o estado vai armazenar uma lista de ciclos usando o termo <ICycle[]>
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-
+  const { createNewCycle, interruptCurrentCycle, activeCycles } =
+    useContext(CyclesContext);
   const newCycleForm = useForm<INewCicleFormatData>({
     resolver: zodResolver(newCycleFormValidationSchema),
 
@@ -54,53 +35,6 @@ export function Home() {
     },
   });
   const { handleSubmit, watch, reset } = newCycleForm;
-  function setSecondsPassed(seconds: number) {
-    setAmountSecondsPassed(seconds);
-  }
-
-  function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() };
-        } else {
-          return cycle;
-        }
-      })
-    );
-  }
-
-  function handleCreateNewCycle(data: INewCicleFormatData) {
-    const id = String(new Date().getTime());
-
-    const newCycle: ICycle = {
-      id,
-      task: data.task,
-      minutesAmount: data.minutesAmount,
-      startDate: new Date(),
-    };
-
-    setCycles((state) => [...state, newCycle]); //coloquei colchetes pq é um array de ciclos, e para adicionar um novo ciclo é necessario os tres pontos para que usem os possiveis ciclos anteriores e depois crie um novo com o newCycle
-    //toda vez que eu estou alterando um estado, e esse estado depende da sua informação anterior, é bom o estado ser setado em formato de função, assim como esta a arrow function acima
-    //state = Estado atual ( poderia ser qualquer nome)
-    setActiveCycleId(id);
-    setAmountSecondsPassed(0); // quando eu crio um novo ciclo, agora eu sempre volto a os segundos que se passaram para zero, para que o ciclo não comece de uma conta errada, do ciclo anterior
-    reset();
-  }
-
-  function handleInterruptCycle() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptDate: new Date() };
-        } else {
-          return cycle;
-        }
-      })
-    );
-    setActiveCycleId(null);
-  }
-  const activeCycles = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const task = watch("task");
 
@@ -108,31 +42,21 @@ export function Home() {
 
   return (
     <HomeContainer>
-      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <CyclesContext.Provider
-          value={{
-            activeCycles,
-            activeCycleId,
-            markCurrentCycleAsFinished,
-            amountSecondsPassed,
-            setSecondsPassed,
-          }}
-        >
-          <FormProvider {...newCycleForm}>
-            <NewCycleForm />
-          </FormProvider>
+      <form action="" onSubmit={handleSubmit(createNewCycle)}>
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
 
-          <Countdown />
-          {activeCycles ? (
-            <StopCountdownButton onClick={handleInterruptCycle} type="button">
-              <HandPalm size={24} /> Interromper
-            </StopCountdownButton>
-          ) : (
-            <StartCountdownButton disabled={isSubmitDisable} type="submit">
-              <Play size={24} /> Começar
-            </StartCountdownButton>
-          )}
-        </CyclesContext.Provider>
+        <Countdown />
+        {activeCycles ? (
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
+            <HandPalm size={24} /> Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisable} type="submit">
+            <Play size={24} /> Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   );
