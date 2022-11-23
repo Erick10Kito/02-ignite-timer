45c0decbd43ce54cb5d120b3ff81a3d15a5e3824
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-23 10:32:16 -0300

    useReducer adicionado

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useReducer, useState } from "react";
 interface ICreateCycleData {
   task: string;
   minutesAmount: number;
@@ -26,6 +26,13 @@ interface ICyclesContextProviderProps {
   children: ReactNode;
 }
 
+// interface IProps {
+//   type?: string;
+//   payload?: {
+//     data?: String[];
+//   };
+// }
+
 export const CyclesContext = createContext({} as ICyclesContextData);
 
 export function CyclesContextProvider({
@@ -33,7 +40,14 @@ export function CyclesContextProvider({
 }: ICyclesContextProviderProps) {
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
   //amountSecondsPassed = segundos que ja se passaram desde que o activeCycles foi ativado
-  const [cycles, setCycles] = useState<ICycle[]>([]); //estou dizendo que o estado vai armazenar uma lista de ciclos usando o termo <ICycle[]>
+  const [cycles, dispatch] = useReducer((state: ICycle[], action: any) => {
+    if (action.type === "ADD_NEW_CYCLE") {
+      console.log(action);
+      return [...state, action.payload.data];
+    }
+    return state;
+  }, []);
+  // const [cycles, setCycles] = useState<ICycle[]>([]); //estou dizendo que o estado vai armazenar uma lista de ciclos usando o termo <ICycle[]>
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
 
   const activeCycles = cycles.find((cycle) => cycle.id === activeCycleId);
@@ -43,15 +57,21 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() };
-        } else {
-          return cycle;
-        }
-      })
-    );
+    dispatch({
+      type: "MARK_CURRENT_CYCLE_AS_FINISHED",
+      payload: {
+        data: activeCycleId,
+      },
+    });
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, finishedDate: new Date() };
+    //     } else {
+    //       return cycle;
+    //     }
+    //   })
+    // );
   }
 
   function createNewCycle(data: ICreateCycleData) {
@@ -64,7 +84,14 @@ export function CyclesContextProvider({
       startDate: new Date(),
     };
 
-    setCycles((state) => [...state, newCycle]); //coloquei colchetes pq é um array de ciclos, e para adicionar um novo ciclo é necessario os tres pontos para que usem os possiveis ciclos anteriores e depois crie um novo com o newCycle
+    dispatch({
+      type: "ADD_NEW_CYCLE",
+      payload: {
+        data: newCycle,
+      },
+    });
+
+    // setCycles((state) => [...state, newCycle]); //coloquei colchetes pq é um array de ciclos, e para adicionar um novo ciclo é necessario os tres pontos para que usem os possiveis ciclos anteriores e depois crie um novo com o newCycle
     //toda vez que eu estou alterando um estado, e esse estado depende da sua informação anterior, é bom o estado ser setado em formato de função, assim como esta a arrow function acima
     //state = Estado atual ( poderia ser qualquer nome)
     setActiveCycleId(id);
@@ -72,15 +99,21 @@ export function CyclesContextProvider({
   }
 
   function interruptCurrentCycle() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptDate: new Date() };
-        } else {
-          return cycle;
-        }
-      })
-    );
+    dispatch({
+      type: "INTERRUPT_CURRENT_CYCLE",
+      payload: {
+        data: activeCycleId,
+      },
+    });
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, interruptDate: new Date() };
+    //     } else {
+    //       return cycle;
+    //     }
+    //   })
+    // );
     setActiveCycleId(null);
   }
   return (
